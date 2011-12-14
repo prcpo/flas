ac6989caa1a46070bf8724d6dd3b42e883814ba3
Alexander Shelemetyev <ex.lxndr@gmail.com>
2011-12-14 07:53:30 +0400

    добавлен клиент на Python

---
+++ gui/pyFlas/translations/ru_RU.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="freiebuch.py" line="187"/>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="freiebuch.py" line="196"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Справка</translation>
+    </message>
+    <message>
+        <location filename="freiebuch.py" line="116"/>
+        <source>FreieBuch</source>
+        <translation>Свободная Бухгалтерия</translation>
+    </message>
+</context>
+</TS>
