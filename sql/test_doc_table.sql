b2b74e466477565441e4b44aa1a12a6e12175514
Alexander Shelemetyev <ex.lxndr@gmail.com>
2011-12-20 12:19:38 +0400

    Исправлено именование элементов

--- sql/test_doc_table.sql
+++ sql/test_doc_table.sql
@@ -6,25 +6,25 @@ CREATE OR REPLACE FUNCTION test_doc_table(text)
   RETURNS text AS
 $BODY$select 
 	json_row(
-		json_element('Caption','Платёжные поручения'::text),
+		json_element('Text','Платёжные поручения'::text),
 		json_element('Table',
 			json_row(
 				json_element('num',
 					json_row(
 						json_element('Type','integer'::text),
-						json_element('Caption','Номер'::text)
+						json_element('Text','Номер'::text)
 					)
 				),
 				json_element('date',
 					json_row(
 						json_element('Type','date'::text),
-						json_element('Caption','Дата'::text)
+						json_element('Text','Дата'::text)
 					)
 				),
 				json_element('sum',
 					json_row(
 						json_element('Type','sum'::text),
-						json_element('Caption','Сумма'::text)
+						json_element('Text','Сумма'::text)
 					)
 				)
 			)
