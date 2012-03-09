62e939d6507f10271252cc8aa2404f3cf7dc3597
Alexander Shelemetyev <ex.lxndr@gmail.com>
2012-03-09 20:23:02 +0400

    Патч, исправляющий ошибки иерархии реквизитов: sql/obj_get.sql sql/req_get.sql

---
+++ sql/obj_get.sql
@@ -0,0 +1,23 @@
+﻿-- Function: obj.get(bigint)
+
+-- DROP FUNCTION obj.get(bigint);
+
+CREATE OR REPLACE FUNCTION obj.get(_id bigint)
+  RETURNS text AS
+$BODY$select
+  json.get(
+    json.element('Type', o.code::text) ||
+    (json.element('UID', o.id::text) ||
+    array(
+              select obj.req_get(id)
+              from obj.req
+              where obj = o.id
+              and parent is null
+    ))
+  )
+from obj.objects o
+where o.id = $1$BODY$
+  LANGUAGE sql VOLATILE
+  COST 100;
+ALTER FUNCTION obj.get(bigint)
+  OWNER TO postgres;
---
+++ sql/req_get.sql
@@ -0,0 +1,33 @@
+﻿-- Function: obj.req_get(bigint)
+
+-- DROP FUNCTION obj.req_get(bigint);
+
+CREATE OR REPLACE FUNCTION obj.req_get(_id bigint)
+  RETURNS text AS
+$BODY$declare
+  _res text;
+  _name text;
+begin
+  select 
+    code::text,
+    val 
+  into _name, _res
+  from obj.req 
+  where id = _id;
+
+  if exists (select 'x' from obj.req where parent = _id or obj = _id) then
+    _res := json.get(
+        json.element('UID', _id::text) ||
+        array(
+          select obj.req_get(id)
+          from obj.req
+          where parent = _id or obj = _id
+        )
+      );
+  end if;
+  return json.element(_name, _res);
+end;$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION obj.req_get(bigint)
+  OWNER TO postgres;
